07a94602ba80d8c2a1d832206e285223dc38540b
DmV554 <daniel3318bl@gmail.com>
2025-06-04 20:58:58 -0400

    Creación carpeta backend y separación de responsabilidades

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('¡Hola Mundo con Express!');
+});
+
+app.get('/api/saludo', (req, res) => {
+  res.json({ mensaje: '¡Hola desde la API!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor Express escuchando en el puerto ${PORT}`);
+  console.log(`Visita http://localhost:${PORT} en tu navegador`);
+});
